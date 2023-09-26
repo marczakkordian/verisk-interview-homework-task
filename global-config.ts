8d7e11fbe65f793a8435bb192c2fb2128a7b89f5
marczakkordian <marczakkordian@gmail.com>
2023-09-26 03:01:41 +0200

    test: added test skeleton and refactor

---
+++ global-config.ts
@@ -0,0 +1,4 @@
+export const globalConfig = {
+  TEST_USER_EMAIL: process.env.TEST_USER_EMAIL,
+  TEST_USER_PASSWORD: process.env.TEST_USER_PASSWORD,
+};
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,69 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
+import { globalConfig } from "../global-config";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.describe("Recruitment task", () => {
+  const userEmail: string = globalConfig.TEST_USER_EMAIL as string;
+  const userPassword: string = globalConfig.TEST_USER_PASSWORD as string;
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  test.beforeEach(async ({ page }) => {
+    const emailInput = page.getByLabel("Email", { exact: true });
+    const passwordInput = page.getByLabel("Password");
+    const signInButton = page.getByRole("button", { name: "Sign In" });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+    await page.goto("./customer/account/login");
+    await emailInput.fill(userEmail);
+    await passwordInput.fill(userPassword);
+    await signInButton.click();
+  });
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  test.afterEach(async ({ page }) => {
+    const userMenuDropdown = page
+      .getByRole("banner")
+      .locator("button")
+      .filter({ hasText: "Change" });
+    const signOutButton = page.getByRole("link", { name: "Sign Out" });
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    await userMenuDropdown.click();
+    await signOutButton.click();
+  });
+
+  test("should add an example product to the cart and remove it", async ({
+    page,
+  }) => {
+    // Given
+    const productName: string = "Luma Analog Watch";
+    const searchProductField = page.getByPlaceholder(
+      "Search entire store here..."
+    );
+    const productLink = page
+      .getByRole("link", { name: `${productName}` })
+      .first();
+    const addProductButton = page.getByRole("button", { name: "Add to Cart" });
+    const notificationMessage = page.getByRole("alert");
+    const shoppingCartLink = page.getByRole("link", { name: "shopping cart" });
+    const productDetailsTable = page.locator("#shopping-cart-table");
+    const removeButtonItem = page.getByTitle("Remove item");
+    // When
+    await searchProductField.fill(productName);
+    await searchProductField.press("Enter");
+    await productLink.click();
+    await addProductButton.click();
+    // Then
+    await expect(notificationMessage).toHaveText(
+      "You added Luma Analog Watch to your shopping cart."
+    );
+    // When
+    await shoppingCartLink.click();
+    // Then
+    await expect(productDetailsTable).toHaveText(`${productName}`);
+    // When
+    await removeButtonItem.click();
+    // Then
+    await page.waitForLoadState();
+    await expect(
+      page.getByText(
+        "You have no items in your shopping cart. Click here to continue shopping."
+      )
+    ).toBeVisible();
+  });
 });
